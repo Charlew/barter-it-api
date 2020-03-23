52b0ed52b2a3960f2387139acb65c7039e747c5a
Karol Wisniewski <karol.wisniewski@ecom.software>
2020-03-23 15:54:14 +0100

    Validate requests
    - add condition and mark to item request

--- src/main/kotlin/barter/barter_it_api/domain/Item.kt
+++ src/main/kotlin/barter/barter_it_api/domain/Item.kt
@@ -3,12 +3,15 @@ package barter.barter_it_api.domain
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 import java.time.LocalDateTime
+import javax.validation.constraints.*
 
 @Document
 data class Item(@Id val id: String? = null,
-                val name: String,
+                val name: String?,
                 val description: String?,
-                val category: Categories,
+                val category: Categories?,
+                val condition: Conditions?,
+                val mark: String?,
                 val createdAt: LocalDateTime = LocalDateTime.now(),
                 val count: Int? = 1)
 
@@ -21,15 +24,35 @@ enum class Categories {
     FURNITURE
 }
 
-data class ItemRequest(val name: String,
-                       val description: String?,
-                       val category: Categories,
-                       val count: Int? = 1) {
+enum class Conditions {
+    VERY_GOOD,
+    GOOD,
+    DAMAGED
+}
+
+data class ItemRequest(
+        @get:NotBlank(message = "notBlank")
+        val name: String?,
+
+        val description: String?,
+
+        @get:NotNull(message = "required")
+        val category: Categories?,
+
+        @get:NotNull(message = "required")
+        val condition: Conditions?,
+
+        val mark: String?,
+
+        @get:Min(value = 1, message = "tooSmall")
+        val count: Int? = 1) {
 
     fun toItem() = Item(
             name = name,
             description = description,
             category = category,
+            condition = condition,
+            mark = mark,
             count = count
     )
 }

a2b173277c135fbbaeb738b21590397cfb2a816e
Karol Wisniewski <karol.wisniewski@ecom.software>
2020-04-14 20:15:29 +0200

    Allow cors filtering for all origins in case of testing

--- src/main/kotlin/barter/barter_it_api/api/user/AuthEndpoint.kt
+++ src/main/kotlin/barter/barter_it_api/api/user/AuthEndpoint.kt
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @Validated
-@CrossOrigin(origins = ["http://localhost:3000", "http://barter-it-web.herokuapp.com"])
+@CrossOrigin(origins = ["*"])
 @RestController
 class AuthEndpoint(
         private val authService: AuthService,

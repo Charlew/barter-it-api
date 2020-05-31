a7db603d79708f7c4d6c6391a947384aa175047d
Filip Wicher <filip.wicher@allegro.pl>
2020-05-31 19:05:57 +0200

    Remove ".*" import

--- src/test/groovy/barter/barter_it_api/api/item/ItemEndpointSpec.groovy
+++ src/test/groovy/barter/barter_it_api/api/item/ItemEndpointSpec.groovy
@@ -9,7 +9,7 @@ import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.test.web.servlet.MockMvc
 import spock.lang.Specification
 
-import static org.springframework.http.MediaType.*
+import static org.springframework.http.MediaType.APPLICATION_JSON
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
